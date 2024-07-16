aa72cb9bd037f2439ee0ebc8ff0a8e5baa3452e5
DvPandya-15 <dvpandya2138433@gmail.com>
2024-07-16 17:20:00 +0530

    chore: add new table

---
+++ supabase/migrations/20240716112932_new_employee.sql
@@ -0,0 +1,56 @@
+create table "public"."employees" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "name " text
+);
+
+
+alter table "public"."employees" enable row level security;
+
+CREATE UNIQUE INDEX employees_pkey ON public.employees USING btree (id);
+
+alter table "public"."employees" add constraint "employees_pkey" PRIMARY KEY using index "employees_pkey";
+
+grant delete on table "public"."employees" to "anon";
+
+grant insert on table "public"."employees" to "anon";
+
+grant references on table "public"."employees" to "anon";
+
+grant select on table "public"."employees" to "anon";
+
+grant trigger on table "public"."employees" to "anon";
+
+grant truncate on table "public"."employees" to "anon";
+
+grant update on table "public"."employees" to "anon";
+
+grant delete on table "public"."employees" to "authenticated";
+
+grant insert on table "public"."employees" to "authenticated";
+
+grant references on table "public"."employees" to "authenticated";
+
+grant select on table "public"."employees" to "authenticated";
+
+grant trigger on table "public"."employees" to "authenticated";
+
+grant truncate on table "public"."employees" to "authenticated";
+
+grant update on table "public"."employees" to "authenticated";
+
+grant delete on table "public"."employees" to "service_role";
+
+grant insert on table "public"."employees" to "service_role";
+
+grant references on table "public"."employees" to "service_role";
+
+grant select on table "public"."employees" to "service_role";
+
+grant trigger on table "public"."employees" to "service_role";
+
+grant truncate on table "public"."employees" to "service_role";
+
+grant update on table "public"."employees" to "service_role";
+
+
